544f6cf3d9910268dfe5634bc59f43a705e4a73b
Sailathapeddireddy <sailathapeddireddy@gmail.com>
2024-07-06 23:00:07 +0530

    first commit

---
+++ ultra1.ino
@@ -0,0 +1,44 @@
+const int pingPin = 3;
+const int echoPin=2;
+
+void setup() {
+ 
+  Serial.begin(9600);
+  pinMode(pingPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+
+  long duration, inches, cm;
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pingPin, LOW);
+
+ 
+  
+  duration = pulseIn(echoPin, HIGH);
+
+  // convert the time into a distance
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+
+  delay(100);
+}
+
+long microsecondsToInches(long microseconds) {
+ 
+  return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  return microseconds / 29 / 2;
+}
